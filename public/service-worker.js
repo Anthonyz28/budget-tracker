038532002c96c6f2d03d8a99a6c9722a77a696ae
Anthonyz28 <anthony.z28@hotmail.com>
2021-04-25 16:19:53 -0500

    fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,23 @@
-const CACHE_NAME = 'my-site-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const CACHE_NAME = 'budget-tracker-cache-v1';
+const DATA_CACHE_NAME = 'budget-data-cache-v1';
 
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
-    '/favicon.ico',
-    '/manifest.json',
-    '/js/index.js',
-    '/js/idb.js',
-    '/css/style.css',
-    '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png',
+    './index.html',
+    './manifest.json',
+
+    './js/index.js',
+    './js/idb.js',
+    './css/style.css',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', function(evt) {
