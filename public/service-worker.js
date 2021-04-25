f5640b52b1fb77524aa3d7b4f040e27af3a647d9
Anthonyz28 <anthony.z28@hotmail.com>
2021-04-25 16:28:57 -0500

    more fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,6 @@ const FILES_TO_CACHE = [
     './js/index.js',
     './js/idb.js',
     './css/style.css',
-    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     './icons/icon-72x72.png',
     './icons/icon-96x96.png',
     './icons/icon-128x128.png',
